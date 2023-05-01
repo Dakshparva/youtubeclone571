f5bd77cce2bc53561d4e4ec39944532b203f93be
Dakshathkumar N <101206655+Dakshparva@users.noreply.github.com>
2023-05-01 14:33:18 +0530

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+        "149a2ed646mshecf4ff4442c10f7p1de5e7jsnefc11445a638",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
